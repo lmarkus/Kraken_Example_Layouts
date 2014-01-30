179cef8db34eb00d978d3a5c42854890cfb10dab
lmarkus <lmarkus@paypal.com>
2014-01-30 12:35:39 -0800

    Make the controller switch layouts based on input

--- controllers/index.js
+++ controllers/index.js
@@ -8,11 +8,18 @@ module.exports = function (app) {
 
     var model = new IndexModel();
 
-
     app.get('/', function (req, res) {
-        
+
+        //Retrieve the "year" parameter from the request. If nothing is present, default to 1994
+        var year = req.param('year') || '1994';
+
+        //Set the layout to be used.
+        model.layout='layouts/'+year;
+        model.year = year;
+
+        //Render the index template using the appropriate layout.
         res.render('index', model);
-        
+
     });
 
 };
